7ab969470df333f73b721a271daf1ada1647c4a8
lz100 <lz100@users.noreply.github.com>
2026-03-20 19:27:35 +0000

    Release 4.0.2

--- R/data.R
+++ R/data.R
@@ -55,3 +55,29 @@
 #'          |- ...
 #' ```
 "lst_sessions"
+
+#' List of latest session mappings
+#'
+#' @format A list mapping each study and release to the latest available
+#'   session metadata used by NBDCtools. First level of the list is the study
+#'   name (currently ABCD & HBCD), and the second level is the
+#'   version/release of the study. Each release contains:
+#'   \describe{
+#'     \item{table_name}{Character scalar with the table used for lookup.}
+#'     \item{session}{Character scalar with the latest session label.}
+#'   }
+#'
+#' ```
+#' - lst_session_latest
+#'     |- abcd
+#'          |- 6.0
+#'          |   |- table_name: "ab_g_dyn"
+#'          |   |- session: "ses-06A"
+#'          |- ...
+#'     |- hbcd
+#'          |- 1.0
+#'          |   |- table_name: "par_visit_data"
+#'          |   |- session: "ses-V03"
+#'          |- ...
+#' ```
+"lst_session_latest"
